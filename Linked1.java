27cf4b146868d3823ba1a073c04c1ed8a7a0629d
Aman dhoni <parleaman@gmail.com>
2025-09-20 19:52:59 +0530

    addbinary

--- Linked1.java
+++ Linked1.java
@@ -1,51 +1,55 @@
-class Node{
+class Node {
     int data;
     Node next;
 
-    Node(int data){
-        this.data=data;
-        this.next=null;
+    Node(int data) {
+        this.data = data;
+        this.next = null;
     }
 }
 
 public class Linked1 {
     Node head;
-    public void  add(int data){
-        Node newNode=new Node(data);
-        if(head==null){
-            head=newNode;
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
             return;
         }
-        Node current=head;
-      while(current.next!=null){
-        current=current.next;
-      }
-      current.next=newNode;
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
     }
-     public void display(){
-        Node current=head;
-        while(current!=null){
-            System.out.print(current.data+" -> ");
-            current=current.next;
+
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
         }
         System.out.println("null");
     }
-    public void addFirst(int data){
-        
-        Node newNode=new Node(data);
-        if(head==null){
-         head=newNode;
-         return;
+
+    public void addFirst(int data) {
+
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
         }
-        newNode.next=head;
-        head=newNode;
+        newNode.next = head;
+        head = newNode;
     }
-   public static void main(String[] args) {
-    Linked1 ll=new Linked1();
-    ll.add(34);
-    ll.add(54);
-    ll.addFirst(22);
-    ll.add(67);
-    ll.display();
-   }    
+
+    public static void main(String[] args) {
+        Linked1 ll = new Linked1();
+        ll.add(34);
+        ll.add(54);
+        ll.addFirst(22);
+        ll.add(67);
+        ll.display();
+    }
 }
